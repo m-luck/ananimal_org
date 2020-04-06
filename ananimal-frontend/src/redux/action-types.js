c5584d0eb8a1869f00f2a8cb58a56817e0067956
Michael Luckyman <lukiman@quorum.us>
2020-04-05 20:02:05 -0700

    Split into working trees.

--- ananimal-frontend/src/redux/action-types.js
+++
@@ -1,6 +0,0 @@
-// Action types are string constants that are to be used as the `type` in Redux Action objects. This is to identify each action when sent to reducers.
-export const DISABLE_NIGHT_MODE = 'DISABLE_NIGHT_MODE'
-export const ENABLE_NIGHT_MODE = 'ENABLE_NIGHT_MODE'
-export const TOGGLE_NIGHT_MODE = 'TOGGLE_NIGHT_MODE'
-
-export const POST_SUBMISSION = 'POST_SUBMISSION'
